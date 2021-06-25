4df9cf0cf170a6e766f81423967b5ba31c134d91
Luluk Mufida <lulukmufida0015@gmail.com>
2021-06-25 15:54:36 +0700

    pertemuan 3 ke-1

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,9 @@
+@extends('utama')
+
+@section('title', 'About Us')
+
+@section('content')
+    @php
+        echo "1941720052, Luluk Mufida";
+    @endphp
+@endsection
---
+++ resources/views/news.blade.php
@@ -0,0 +1,10 @@
+@extends('utama')
+@section('title', 'News')
+@parent
+
+@section('content')
+<h1>News</h1>
+<a href="https://www.educastudio.com/'.$param.'" target="_blank">https://www.educastudio.com/'.$param.'</a><br>
+<a href="https://www.educastudio.com/news/'.$param.'" target="_blank">https://www.educastudio.com/news/'.$param.'</a><br>
+    
+@endsection
